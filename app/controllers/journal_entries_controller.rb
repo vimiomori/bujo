fee255a4b565c3f6e66554041e542ffa704034af
vimiomori <vivian.muchen@gmail.com>
2020-03-13 13:54:38 +0900

    add partials and destroy action

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,6 +1,14 @@
 class JournalEntriesController < ApplicationController
 
+  def index
+    @journal_entries = JournalEntry.all
+  end
+
   def show
+    @journal_entry = JournalEntry.find(params[:id])
+  end
+
+  def edit
     @journal_entry = JournalEntry.find(params[:id])
   end
   
@@ -15,6 +23,23 @@ class JournalEntriesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update 
+    @journal_entry = JournalEntry.find(params[:id])
+    if @journal_entry.update(journal_entry_params)
+      flash[:notice] = "Journal entry updated!"
+      redirect_to @journal_entry
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @journal_entry = JournalEntry.find(params[:id])
+    @journal_entry.destroy
+    flash[:notice] = "Journal entry deleted!"
+    redirect_to journal_entries_path
   end
 
   private
