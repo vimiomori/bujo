a0e2de23a7ce285884affd1389a8eb567a315569
vimiomori <vivian.muchen@gmail.com>
2020-03-17 14:06:28 +0900

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do |user|
+  User.create!(
+    username: "user##{user}",
+    email: "user#{user}@email.com",
+    password: "password#{user}"
+  )
+end
+
+puts "20 users created"
+
+User.all.each do |user|
+  20.times do |journal_entry|
+    JournalEntry.create!(
+      title: "My Journal Entry ##{journal_entry}",
+      content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+      date: Date.today.to_s(:db),
+      time: Time.now,
+      user_id: user.id
+    )
+  end
+  puts "20 blog posts created for #{user.username}"
+end
